67cf628c9c1e5171e026a6253810e86bbdd7f04c
Arasakki <pacificsalvador999@gmail.com>
2023-11-07 09:56:04 +0500

    1

--- src/read_message/read.tsx
+++ src/read_message/read.tsx
@@ -24,9 +24,9 @@ function Read() {
                             <div className="header_block_read">
                                 <h1>Message</h1>
                             </div>
-                            <div className="read_button" style={{marginBottom: '46px'}}>
-                                <button className="button_active">
-                                    <Link to="/readmsg" style={{textDecoration: 'none', color: 'white', display: 'flex', alignItems: 'center'}}>
+                            <div className="read_button">
+                                <button className="button_active read_button_Link">
+                                    <Link to="/readmsg" >
                                         Click to read message 
                                         <svg style={{marginLeft: '30px'}} xmlns="http://www.w3.org/2000/svg" width="61" height="61" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16">
                                             <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
@@ -34,7 +34,7 @@ function Read() {
                                     </Link>
                                 </button>
                             </div>
-                            <p style={{fontSize: '20px', fontWeight: '400', color: '#CCC', marginBottom: '105px'}}>
+                            <p className="read_description" style={{fontSize: '20px', fontWeight: '400', color: '#CCC'}}>
                                 If the link is not valid, follow these short instructions:
                                 <br/><br/>
                                 Check the link:<br/>
